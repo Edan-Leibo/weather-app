7f1a5735a4961b3572d4889edf7115494f27151a
Edan <edan.leibo@gmail.com>
2019-05-07 15:34:37 +0300

    fix celsius to fahrenheit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,17 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/06f7ae2b8c492f63f6455d7eb1bcb2b4/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/06f7ae2b8c492f63f6455d7eb1bcb2b4/' + latitude + ',' + longitude + '?' + 'units=si';
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Connection to weather failed', undefined)
+            callback('Connection to weather failed', undefined);
         } else if (body.error) {
-            callback('Location was not found', undefined)
+            callback('Location was not found', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' The tempature right now is ' + body.currently.temperature + ' degress. And it will be between ' + body.daily.data[0].temperatureHigh + ' to ' + body.daily.data[0].temperatureLow + ', with ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' The tempature right now is ' + body.currently.temperature + ' degress. And it will be between ' + body.daily.data[0].temperatureHigh + ' to ' + body.daily.data[0].temperatureLow + ', with ' + body.currently.precipProbability + '% chance of rain.');
         }
     })
 }
 
-module.exports = forecast
+module.exports = forecast;
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,13 +1,13 @@
-const request = require('request')
+const request = require('request');
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZWRhbiIsImEiOiJjanZkbHY4a3QwZWd2NGRvNmF3cmtvajkwIn0.Z5Dq656M8djolcQZL2dexQ&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZWRhbiIsImEiOiJjanZkbHY4a3QwZWd2NGRvNmF3cmtvajkwIn0.Z5Dq656M8djolcQZL2dexQ&limit=1';
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Connection to weather failed', undefined)
+            callback('Connection to weather failed', undefined);
         } else if (body.features.length === 0) {
-            callback('Location was not found.', undefined)
+            callback('Location was not found.', undefined);
         } else {
             callback(undefined, {
                 latitude: body.features[0].center[1],
@@ -18,4 +18,4 @@ const geocode = (address, callback) => {
     })
 }
 
-module.exports = geocode
+module.exports = geocode;
