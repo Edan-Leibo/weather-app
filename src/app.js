3626a2d9fc410de93589fcc2d24f8fe034244493
Edan <edan.leibo@gmail.com>
2019-05-07 13:20:11 +0300

    add test route

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-
+app.get('/', (req, res) => {
+    res.send("hi!!");
+})
 
 app.listen(port, () => {
     console.log('Server is running with port ' + port)
