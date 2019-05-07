7713c145117988756974c34111361cdcabd348e5
Edan <edan.leibo@gmail.com>
2019-05-07 14:41:50 +0300

    add views

--- src/app.js
+++ src/app.js
@@ -1,48 +1,48 @@
-const express = require('express')
-const forecast = require('./utils/forecast')
-const geocode = require('./utils/geocode')
-const hbs = require('hbs')
-const path = require('path')
+const express = require('express');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
+const hbs = require('hbs');
+const path = require('path');
 
 const app = express()
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Weather'
-    })
+    });
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-    })
+    });
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-    })
-})
+    });
+});
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
             error: 'Address must be provided'
-        })
+        });
     }
 
     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
@@ -71,18 +71,18 @@ app.get('/help/*', (req, res) => {
         title: '404',
         errorMessage: 'Help article not found.'
     })
-})
+});
 
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
         errorMessage: 'Page not found.'
     })
-})
+});
 
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log('Server is running with port ' + port)
-})
+});
